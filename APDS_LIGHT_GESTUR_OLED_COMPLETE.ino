cae0b3cab2d729594f6256c4bf5f7f3513addf68
yyc9920 <53812519+yyc9920@users.noreply.github.com>
2019-10-19 10:28:54 +0900

    Update APDS_LIGHT_GESTUR_OLED_COMPLETE.ino
    - 기능1 : 제스처 기능을 활성화 하여 손을 상,하,좌,우로 움직 일때 의 동작 상태를 LCD 로 출력 하시오.
    - 기능2 : 조도 센서로 활용하여 일정 값이상 작아 졌을때 LED로 동작 확인을 수행 하시오.(LED 포트 자율)
    - 기능간 변환 동작은 G0의 스위치를 활용하여 할 것.

--- APDS_LIGHT_GESTUR_OLED_COMPLETE.ino
+++ APDS_LIGHT_GESTUR_OLED_COMPLETE.ino
@@ -11,10 +11,10 @@
   BSD license, all text above must be included in any redistribution
  ***************************************************************************/
 
-int switch1;
-int Key_off_flag = 0;
-int adps_mode = 0;
-char buf1[32];
+int switch1;                      //스위치 값을 저장할 변수
+int Key_off_flag = 0;             //스위치 채터링을 방지할 플래그
+int adps_mode = 0;                //제스쳐 센서 모드와 조도 센서 모드를 판별할 변수
+char buf1[32];                    //OLED에 표시할 내용을 저장할 문자 배열
 char buf2[32];
 #include <Wire.h>
 #include "Adafruit_APDS9960.h"
@@ -24,15 +24,15 @@ OLED display(4,5);
 
 void setup() {
   display.begin();
-  pinMode(0, INPUT_PULLUP);
-  pinMode(2, OUTPUT);
+  pinMode(0, INPUT_PULLUP);       //G0를 입력으로
+  pinMode(2, OUTPUT);             //2번 LED를 출력으로
 
-  apds.begin();
+  apds.begin();                   //APDS9960 사용
 }
 
 void loop() {
-  switch1 = digitalRead(0);
-  if(switch1 == 0)
+  switch1 = digitalRead(0);       //스위치 값을 읽어 변수에 저장
+  if(switch1 == 0)                //채터링 방지
   {
     if(Key_off_flag == 1)
       {
@@ -43,35 +43,34 @@ void loop() {
   else {
     Key_off_flag = 1;
   }
-  if (adps_mode >= 2) adps_mode = 0;
+  if (adps_mode >= 2) adps_mode = 0;    //모드 0 -> 조도센서, 모드 1 -> 제스쳐 센서
   
-  if (adps_mode == 0) {
-    apds.enableGesture(false);
-    delay(10);
+  if (adps_mode == 0) {           //조도센서 모드
+    apds.enableGesture(false);    //조도센서를 활성화 할 때에 다른 센서와 데이터 충돌이 일어나기 때문에
+    delay(10);                    //조도센서의 경우엔 다른 센서를 off한 상태에서 작동시켜야 함.
     apds.enableProximity(false);
     delay(10);
     apds.enableColor(true);
     delay(10);
     uint16_t r, g, b, c;
     float illuminance;
-    //get the data and print the different channels
     apds.getColorData(&r, &g, &b, &c);
     sprintf(buf1, "Light Sensor  ");
-    display.print(buf1, 0, 0);
+    display.print(buf1, 0, 0);    //OLED에 표시
     display.print("        ", 1, 0);
-    illuminance = (float)apds.calculateLux(r, g, b);
-    if (illuminance > 35) digitalWrite(2, LOW);
-    else digitalWrite(2, HIGH);
+    illuminance = (float)apds.calculateLux(r, g, b);    //밝기를 측정
+    if (illuminance > 35) digitalWrite(2, LOW);         //밝을때는 LED ON
+    else digitalWrite(2, HIGH);                         //어두울때는 LED OFF
   }
 
   if (adps_mode == 1) {
-    apds.enableGesture(true);
+    apds.enableGesture(true);     //제스쳐 모드 활성화
     delay(1);
-    apds.enableProximity(true);
+    apds.enableProximity(true);   //제스쳐 모드는 Proximity(원근 센서)와 함께 동작함.
     delay(1);
-    digitalWrite(2, HIGH);
-    uint8_t gesture = apds.readGesture();
-    sprintf(buf1, "Gesture Sensor");
+    digitalWrite(2, HIGH);        //LED는 OFF
+    uint8_t gesture = apds.readGesture();   //제스쳐 값을 읽어서 변수에 저장
+    sprintf(buf1, "Gesture Sensor");  //OLED에 제스쳐 값을 표시
     display.print(buf1, 0, 0);
     if(gesture == APDS9960_DOWN) display.print("Dir : v", 1, 0);
     if(gesture == APDS9960_UP) display.print("Dir : ^", 1, 0);
